4abe69f96f3f4a6855f98bb271668c25cc5dd659
wangcheng <1223231151@qq.com>
2024-01-24 02:57:38 +0800

    初步加入了Odrive的驱动文件

--- User_Core/Inc/odrive.h
+++ User_Core/Inc/odrive.h
@@ -3,6 +3,9 @@
 
 #include "main.h"
 
+/**
+ * 输出速度模式所用的结构体
+ */
 
 typedef union
 {
@@ -12,6 +15,22 @@ typedef union
     uint32_t data_uint;
 }union_32;
 
+/**
+ * 输出位置模式所用的结构体
+ */
+
+typedef union
+{
+    uint8_t data_8[4];
+    float data_f;
+    int16_t data_v;
+    int16_t data_t;
+}union_16;
+
+/**
+ * RM电机接收所用的结构体
+ */
+
 typedef union
 {
     uint8_t data_u8[8];
@@ -19,6 +38,14 @@ typedef union
     int data_i32[2];
     uint32_t data_u32[2];
 }union_64;
+
+/*********
+ *
+ * 在标准CAN通信中Odrive驱动电机时CAN ID的部分中bit10-bit5的部分为存放电机ID的部分，
+ * 即为下方的宏定义，其中Axis1_ID 对应的是电机id为1时，id移位五位后的结果，
+ * 注意：这里的id为1是十进制而非十六进制。
+ *
+ *********/
 
 #define Axis1_ID 0x020		//1<<5
 #define Axis2_ID 0x040		//2<<5
@@ -29,42 +56,119 @@ typedef union
 #define Axis7_ID 0x0E0		//7<<5
 #define Axis8_ID 0x100		//8<<5
 
-#define Axis0_Get_Motor_Error 		 Axis0_ID + 0x002
+/**
+ * 让八个电机停止转动，是MCU发给电机
+ */
+
 #define Axis1_Get_Motor_Error 		 Axis1_ID + 0x002
+#define Axis2_Get_Motor_Error 		 Axis2_ID + 0x002
+#define Axis3_Get_Motor_Error 		 Axis3_ID + 0x002
+#define Axis4_Get_Motor_Error 		 Axis4_ID + 0x002
+#define Axis5_Get_Motor_Error 		 Axis5_ID + 0x002
+#define Axis6_Get_Motor_Error 		 Axis6_ID + 0x002
+#define Axis7_Get_Motor_Error 		 Axis7_ID + 0x002
+#define Axis8_Get_Motor_Error 		 Axis8_ID + 0x002
 
-#define Axis0_Get_Encoder_Error  	 Axis0_ID + 0x004
+/**
+ * 检测八个电机的编码器是否出问题，是电机发给MCU
+ */
+
 #define Axis1_Get_Encoder_Error 	 Axis1_ID + 0x004
+#define Axis2_Get_Encoder_Error 	 Axis2_ID + 0x004
+#define Axis3_Get_Encoder_Error 	 Axis3_ID + 0x004
+#define Axis4_Get_Encoder_Error 	 Axis4_ID + 0x004
+#define Axis5_Get_Encoder_Error 	 Axis5_ID + 0x004
+#define Axis6_Get_Encoder_Error 	 Axis6_ID + 0x004
+#define Axis7_Get_Encoder_Error 	 Axis7_ID + 0x004
+#define Axis8_Get_Encoder_Error 	 Axis8_ID + 0x004
 
-#define Set_Axis0_Requested_State    Axis0_ID + 0x007
-#define Set_Axis1_Requested_State    Axis1_ID + 0x007
+/**
+ * 设置八个电机的设置模式，是MCU发给电机
+ * 我们应当只使用=8时候的闭环控制模式
+ */
 
-#define Set_Axis0_Controller_Modes   Axis0_ID + 0x00B
+#define Set_Axis1_Requested_State 	 Axis1_ID + 0x007
+#define Set_Axis2_Requested_State 	 Axis2_ID + 0x007
+#define Set_Axis3_Requested_State 	 Axis3_ID + 0x007
+#define Set_Axis4_Requested_State 	 Axis4_ID + 0x007
+#define Set_Axis5_Requested_State 	 Axis5_ID + 0x007
+#define Set_Axis6_Requested_State 	 Axis6_ID + 0x007
+#define Set_Axis7_Requested_State 	 Axis7_ID + 0x007
+#define Set_Axis8_Requested_State 	 Axis8_ID + 0x007
+
+/**
+ * 设置八个电机的控制模式，是MCU发给电机
+ * =0时为电压控制模式
+ * =1时为力矩控制模式
+ * =2时为速度控制模式
+ * =3时为位置控制模式
+ */
+
 #define Set_Axis1_Controller_Modes   Axis1_ID + 0x00B
+#define Set_Axis2_Controller_Modes   Axis2_ID + 0x00B
+#define Set_Axis3_Controller_Modes   Axis3_ID + 0x00B
+#define Set_Axis4_Controller_Modes   Axis4_ID + 0x00B
+#define Set_Axis5_Controller_Modes   Axis5_ID + 0x00B
+#define Set_Axis6_Controller_Modes   Axis6_ID + 0x00B
+#define Set_Axis7_Controller_Modes   Axis7_ID + 0x00B
+#define Set_Axis8_Controller_Modes   Axis8_ID + 0x00B
 
-#define Set_Axis0_Set_Input_Pos      Axis0_ID + 0x00C
+/**
+ *设置目标位置以及前馈速度与前馈力矩
+ */
+
 #define Set_Axis1_Set_Input_Pos      Axis1_ID + 0x00C
+#define Set_Axis2_Set_Input_Pos      Axis2_ID + 0x00C
+#define Set_Axis3_Set_Input_Pos      Axis3_ID + 0x00C
+#define Set_Axis4_Set_Input_Pos      Axis4_ID + 0x00C
+#define Set_Axis5_Set_Input_Pos      Axis5_ID + 0x00C
+#define Set_Axis6_Set_Input_Pos      Axis6_ID + 0x00C
+#define Set_Axis7_Set_Input_Pos      Axis7_ID + 0x00C
+#define Set_Axis8_Set_Input_Pos      Axis8_ID + 0x00C
 
-#define Set_Axis0_Set_Input_Vel      Axis0_ID + 0x00D
+/**
+ *设置目标速度以及前馈力矩
+ */
+
 #define Set_Axis1_Set_Input_Vel      Axis1_ID + 0x00D
+#define Set_Axis2_Set_Input_Vel      Axis2_ID + 0x00D
+#define Set_Axis3_Set_Input_Vel      Axis3_ID + 0x00D
+#define Set_Axis4_Set_Input_Vel      Axis4_ID + 0x00D
+#define Set_Axis5_Set_Input_Vel      Axis5_ID + 0x00D
+#define Set_Axis6_Set_Input_Vel      Axis6_ID + 0x00D
+#define Set_Axis7_Set_Input_Vel      Axis7_ID + 0x00D
+#define Set_Axis8_Set_Input_Vel      Axis8_ID + 0x00D
 
-#define AXIS_STATE_UNDEFINED  				0
-#define AXIS_STATE_IDLE  					1
-#define AXIS_STATE_STARTUP_SEQUENCE  		2
-#define AXIS_STATE_FULL_CALIBRATION_SEQUENCE   	3
-#define AXIS_STATE_MOTOR_CALIBRATION  			4
-#define AXIS_STATE_ENCODER_INDEX_SEARCH  		6
-#define AXIS_STATE_ENCODER_OFFSET_CALIBRATION  	7
-#define AXIS_STATE_CLOSED_LOOP_CONTROL  		8
-#define AXIS_STATE_LOCKIN_SPIN   				9
-#define AXIS_STATE_ENCODER_DIR_FIND  		    10
-#define AXIS_STATE_HOMING  						11
-#define AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION    12
-#define AXIS_STATE_ENCODER_HALL_PHASE_CALIBRATION  		13
+/**
+ *Odrive state 的所含状态
+ */
+
+#define AXIS_STATE_UNDEFINED  				                        0
+#define AXIS_STATE_IDLE  					                        1
+#define AXIS_STATE_STARTUP_SEQUENCE  		                        2
+#define AXIS_STATE_FULL_CALIBRATION_SEQUENCE   	                    3
+#define AXIS_STATE_MOTOR_CALIBRATION  			                    4
+#define AXIS_STATE_ENCODER_INDEX_SEARCH  		                    6
+#define AXIS_STATE_ENCODER_OFFSET_CALIBRATION  	                    7
+#define AXIS_STATE_CLOSED_LOOP_CONTROL  		                    8
+#define AXIS_STATE_LOCKIN_SPIN   				                    9
+#define AXIS_STATE_ENCODER_DIR_FIND  		                        10
+#define AXIS_STATE_HOMING  						                    11
+#define AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION                12
+#define AXIS_STATE_ENCODER_HALL_PHASE_CALIBRATION  		            13
+
+/**
+ * 控制模式所含状态
+ */
 
 #define CONTROL_MODE_VOLTAGE_CONTROL  								0
 #define CONTROL_MODE_TORQUE_CONTROL   								1
 #define CONTROL_MODE_VELOCITY_CONTROL   							2
 #define CONTROL_MODE_POSITION_CONTROL    							3
+
+/**
+ * 输入模式所含状态
+ */
 
 #define INPUT_MODE_INACTIVE    										0
 #define INPUT_MODE_PASSTHROUGH    									1
@@ -77,7 +181,28 @@ typedef union
 #define INPUT_MODE_Tuning    										8
 
 
-void Odrive_Axis0_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
 void Odrive_Axis1_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis2_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis3_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis4_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis5_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis6_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis7_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+void Odrive_Axis8_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque);
+
+void Odrive_Axis1_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis2_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis3_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis4_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis5_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis6_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis7_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+void Odrive_Axis8_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF);
+
+void Odrive_val_output(uint8_t id);
+void AllMotor_valOutput(void);
+
+void Odrive_Postion_output(uint8_t id);
+void AllMotor_PositionOutput(void);
 
 #endif
--- User_Core/Src/odrive.c
+++ User_Core/Src/odrive.c
@@ -11,14 +11,15 @@
 */
 
 union_32 union_32f;
+union_16 union_16f;
 
-void Odrive_Axis0_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+void Odrive_Axis2_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
 {
     CAN_TxHeaderTypeDef TxHeader;
     uint8_t TxData[8];
     uint32_t mbox;         //发送使用到的can邮箱
 
-//    TxHeader.StdId = Set_Axis0_Set_Input_Vel;
+    TxHeader.StdId = Set_Axis2_Set_Input_Vel;
     TxHeader.IDE = 0;
     TxHeader.RTR = 0;
     TxHeader.DLC = 8;
@@ -78,5 +79,573 @@ void Odrive_Axis1_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float 
     }
 }
 
+void Odrive_Axis3_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
 
+    TxHeader.StdId = Set_Axis3_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
 
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+void Odrive_Axis4_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis4_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+void Odrive_Axis5_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis5_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+void Odrive_Axis6_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis6_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+void Odrive_Axis7_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis7_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+void Odrive_Axis8_Set_Input_Vel(CAN_HandleTypeDef *_hcan, float Input_Vel,float Torque)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis8_Set_Input_Vel;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_32f.data_f = Input_Vel;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_32f.data_f = Torque;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+    TxData[6] = union_32f.data_8[2];
+    TxData[7] = union_32f.data_8[3];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis1_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis1_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis2_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis2_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis3_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis3_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis4_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis4_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis5_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis5_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis6_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis6_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis7_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis7_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_Axis8_Set_Input_Position(CAN_HandleTypeDef *_hcan, float Input_Pos,int16_t Vel_FF,int16_t Torque_FF)
+{
+    static CAN_TxHeaderTypeDef TxHeader;
+    static uint8_t TxData[8];
+    static uint32_t mbox;         //发送使用到的can邮箱
+
+    TxHeader.StdId = Set_Axis8_Set_Input_Pos;
+    TxHeader.IDE = 0;
+    TxHeader.RTR = 0;
+    TxHeader.DLC = 8;
+
+    union_16f.data_f = Input_Pos;
+    TxData[0] = union_32f.data_8[0];
+    TxData[1] = union_32f.data_8[1];
+    TxData[2] = union_32f.data_8[2];
+    TxData[3] = union_32f.data_8[3];
+
+    union_16f.data_v = Vel_FF;
+    TxData[4] = union_32f.data_8[0];
+    TxData[5] = union_32f.data_8[1];
+
+    union_16f.data_t = Torque_FF;
+    TxData[6] = union_32f.data_8[0];
+    TxData[7] = union_32f.data_8[1];
+
+    //等一个空の邮箱呢
+    while(HAL_CAN_GetTxMailboxesFreeLevel(_hcan) == 0);
+
+    //发送成功了吗？失败就卡住了捏
+    if (HAL_CAN_AddTxMessage(_hcan, &TxHeader, TxData, &mbox) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Odrive_val_output(uint8_t id)
+{
+    switch(id)
+    {
+        case 1:
+            Odrive_Axis1_Set_Input_Vel(&hcan1,AngleLoop[1].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 2:
+            Odrive_Axis2_Set_Input_Vel(&hcan1,AngleLoop[2].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 3:
+            Odrive_Axis3_Set_Input_Vel(&hcan1,AngleLoop[3].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 4:
+            Odrive_Axis4_Set_Input_Vel(&hcan1,AngleLoop[4].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 5:
+            Odrive_Axis5_Set_Input_Vel(&hcan1,AngleLoop[5].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 6:
+            Odrive_Axis6_Set_Input_Vel(&hcan1,AngleLoop[6].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 7:
+            Odrive_Axis7_Set_Input_Vel(&hcan1,AngleLoop[7].Out_put,0.0f);
+            osDelay(1);
+            break;
+        case 8:
+            Odrive_Axis8_Set_Input_Vel(&hcan1,AngleLoop[8].Out_put,0.0f);
+            osDelay(1);
+            break;
+        default:
+            break;
+    }
+}
+
+void Odrive_Postion_output(uint8_t id)
+{
+    switch(id)
+    {
+        case 1:
+            Odrive_Axis1_Set_Input_Position(&hcan1,AngleWant_MotorX[1],1,0);
+            osDelay(1);
+            break;
+        case 2:
+            Odrive_Axis2_Set_Input_Position(&hcan1,AngleWant_MotorX[2],1,0);
+            osDelay(1);
+            break;
+        case 3:
+            Odrive_Axis3_Set_Input_Position(&hcan1,AngleWant_MotorX[3],1,0);
+            osDelay(1);
+            break;
+        case 4:
+            Odrive_Axis4_Set_Input_Position(&hcan1,AngleWant_MotorX[4],1,0);
+            osDelay(1);
+            break;
+        case 5:
+            Odrive_Axis5_Set_Input_Position(&hcan1,AngleWant_MotorX[5],1,0);
+            osDelay(1);
+            break;
+        case 6:
+            Odrive_Axis6_Set_Input_Position(&hcan1,AngleWant_MotorX[6],1,0);
+            osDelay(1);
+            break;
+        case 7:
+            Odrive_Axis7_Set_Input_Position(&hcan1,AngleWant_MotorX[7],1,0);
+            osDelay(1);
+            break;
+        case 8:
+            Odrive_Axis8_Set_Input_Position(&hcan1,AngleWant_MotorX[8],1,0);
+            osDelay(1);
+            break;
+        default:
+            break;
+    }
+}
+
+void AllMotor_valOutput(void)
+{
+    for(uint8_t Id = 1;Id < 9;Id ++)
+    {
+        Odrive_val_output(Id);
+    }
+}
+
+void AllMotor_PositionOutput(void)
+{
+    for(uint8_t Id = 1;Id < 9;Id ++)
+    {
+        Odrive_Postion_output(Id);
+    }
+}
